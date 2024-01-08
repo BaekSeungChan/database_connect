da9b1240701fee1cd7d94e083005b207815d7d79
baekseungchan1117 <bsc7386@gmail.com>
2024-01-08 23:03:16 +0900

    디펜더시 설정

---
+++ src/test/java/com/example/databasesetting/DatabaseSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasesetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
